8674118e0bf5f7db24afd9dc160dd9f912ec4677
ulol <jeffk0408@naver.com>
2023-03-31 08:08:51 +0900

    feat : ê°: 간단한 파일 입입출력 아카이브 객체 생성 및 사용

--- Source/UnrealSerialize/MyGameInstance.cpp
+++ Source/UnrealSerialize/MyGameInstance.cpp
@@ -3,3 +3,52 @@
 
 #include "MyGameInstance.h"
 
+UMyGameInstance::UMyGameInstance()
+{
+	
+}
+
+void UMyGameInstance::Init()
+{
+	Super::Init();
+	FStudentData RawDataSrc(11, TEXT("abc"));
+
+	// 저장할 파일의 위치
+	const FString SavedDir = FPaths::Combine(FPlatformMisc::ProjectDir(), TEXT("Saved"));
+	UE_LOG(LogTemp, Log, TEXT("저장할 파일위치 : %s"), *SavedDir);
+
+	{
+		const FString RawDataFilename(TEXT("RawData.bin"));
+		FString RawDataAbsolutePath = FPaths::Combine(*SavedDir, *RawDataFilename);
+		UE_LOG(LogTemp, Log, TEXT("저장할 파일 절대경로 : %s"), *RawDataAbsolutePath);
+
+		FPaths::MakeStandardFilename(RawDataAbsolutePath);
+		UE_LOG(LogTemp, Log, TEXT("저장할 파일 절대경로 : %s"), *RawDataAbsolutePath);
+		
+		// Ar는 Archive라는 뜻 ㅎ
+		// 전달해준 Path에 write할 수 있는 아카이브 객체 생성됨.
+		FArchive* RawFileWriterAr = IFileManager::Get().CreateFileWriter(*RawDataAbsolutePath);
+		if (RawFileWriterAr != nullptr)
+		{
+			*RawFileWriterAr << RawDataSrc;
+			RawFileWriterAr->Close();
+			delete RawFileWriterAr;
+			RawFileWriterAr = nullptr;
+
+			// UE_LOG(LogTemp, Log, TEXT("[RawData] 이름 %s 순번 %d"), *RawDa);
+		}
+		FStudentData RawDataDest;
+		FArchive* RawFileReaderAr = IFileManager::Get().CreateFileReader(*RawDataAbsolutePath);
+		if (RawFileReaderAr != nullptr)
+		{
+			// << 연산자라 입력 같지만, Reader에 대한 << 연산자는 Reader에서 Dest로 쓴다.
+			*RawFileReaderAr << RawDataDest;
+			RawFileReaderAr->Close();
+			delete RawFileReaderAr;
+			RawFileReaderAr = nullptr;
+
+			UE_LOG(LogTemp, Log, TEXT("[RawData] 이름 %s 순번 %d"), *RawDataDest.Name, RawDataDest.Order);
+		}
+		
+	}
+}
--- Source/UnrealSerialize/MyGameInstance.h
+++ Source/UnrealSerialize/MyGameInstance.h
@@ -6,6 +6,22 @@
 #include "Engine/GameInstance.h"
 #include "MyGameInstance.generated.h"
 
+
+struct FStudentData
+{
+	FStudentData() {}
+	FStudentData(int32 InOrder, const FString& InName)
+		: Order(InOrder), Name(InName) {}
+
+	int32 Order = -1;
+	FString Name = TEXT("홍길동");
+
+	friend FArchive& operator<<(FArchive& Ar, FStudentData& InStudentData)
+	{
+		Ar << InStudentData.Order << InStudentData.Name; 
+		return Ar;
+	}
+};
 /**
  * 
  */
@@ -13,5 +29,9 @@ UCLASS()
 class UNREALSERIALIZE_API UMyGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
+
+public:
+	UMyGameInstance();
 	
+	virtual void Init() override;
 };
